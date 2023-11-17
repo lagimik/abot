180c898cae8853fcf0ce31b836628a0f953a2c43
Marc-André Morisset <mamoris@microsoft.com>
2023-11-17 10:45:47 -0500

    combined the azure function with the crawler

--- CrucibleCrawler/HttpTrigger.cs
+++ CrucibleCrawler/HttpTrigger.cs
@@ -8,6 +8,12 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
+using Abot2.Core;
+using Abot2.Crawler;
+using Abot2.Poco;
+
+
+
 namespace Abot.Function
 {
     public static class HttpTrigger
@@ -25,9 +31,22 @@ namespace Abot.Function
             dynamic data = JsonConvert.DeserializeObject(requestBody);
             name = name ?? data?.name;
 
+            var config = new CrawlConfiguration
+                {
+                    MaxPagesToCrawl = 25,
+                    MinCrawlDelayPerDomainMilliSeconds = 3000
+                };
+            var crawler = new PoliteWebCrawler(config);
+
+            var crawlResult = await crawler.CrawlAsync(new Uri("https://lagimik.github.io/PartnerCrucible/PracticeBuilding"));
+
+
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
                 : $"Hello, {name}. This HTTP triggered function executed successfully.";
+            
+            responseMessage += crawlResult;
+
 
             return new OkObjectResult(responseMessage);
         }
