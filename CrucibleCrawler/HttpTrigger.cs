eab26d20801e09c0b0775fdc2a585ffe23caac23
Marc-André Morisset <mamoris@microsoft.com>
2023-11-17 11:33:45 -0500

    implemented using page requestor instead

--- CrucibleCrawler/HttpTrigger.cs
+++ CrucibleCrawler/HttpTrigger.cs
@@ -7,7 +7,6 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-
 using Abot2.Core;
 using Abot2.Crawler;
 using Abot2.Poco;
@@ -31,22 +30,16 @@ namespace Abot.Function
             dynamic data = JsonConvert.DeserializeObject(requestBody);
             name = name ?? data?.name;
 
-            var config = new CrawlConfiguration
-                {
-                    MaxPagesToCrawl = 25,
-                    MinCrawlDelayPerDomainMilliSeconds = 3000
-                };
-            var crawler = new PoliteWebCrawler(config);
+            var pageRequester = new PageRequester(new CrawlConfiguration(), new WebContentExtractor());
 
-            var crawlResult = await crawler.CrawlAsync(new Uri("https://lagimik.github.io/PartnerCrucible/PracticeBuilding"));
+            var crawledPage = await pageRequester.MakeRequestAsync(new Uri("http://google.com"));
 
 
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
                 : $"Hello, {name}. This HTTP triggered function executed successfully.";
             
-            responseMessage += crawlResult;
-
+            responseMessage += crawledPage.Content.Text;
 
             return new OkObjectResult(responseMessage);
         }
